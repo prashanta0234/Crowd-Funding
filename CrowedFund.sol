9df8eaf336c2967849bf42158dde4384871ffc61
"Prashanta Chakraborty" <"prashanta0234@gmail.com">
2022-05-01 09:34:04 +0600

    Make a request

--- CrowedFund.sol
+++ CrowedFund.sol
@@ -9,6 +9,19 @@ contract CrowedFund{
     uint public noOfContributors;
     uint public amount;
     mapping(address=>uint) public contributors;
+    // for creating Request
+    struct Request{
+        string description;
+        address payable recipent;
+        uint value;
+        bool complited;
+        uint numOfVoters;
+        mapping(address=>bool) voters;
+    }
+    // for request
+    mapping(uint=>Request) public requests;
+    uint public noOfRequest;
+
     constructor(uint _time,uint _Raisedamount,uint _MinumumContribution){
         manager=msg.sender;
         deadLine=block.timestamp+_time;
@@ -37,6 +50,19 @@ contract CrowedFund{
         address payable user=payable(msg.sender);
         user.transfer(contributors[msg.sender]);
         contributors[msg.sender]=0;
+    }
+    function makeARequest(string memory _description,uint _amount,address _recipent)  public isManger{
+        Request storage newRequest=requests[noOfRequest];
+        noOfRequest++;
+        newRequest.description=_description;
+        newRequest.value=_amount;
+        newRequest.recipent=payable(_recipent);
+        newRequest.complited=false;
+        newRequest.numOfVoters=0;
+
+    }
+    function vote() public{
+        
     }
 
 }
